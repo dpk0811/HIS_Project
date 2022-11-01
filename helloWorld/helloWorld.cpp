537f910118578ab570a62c62b2ae337a3d4c9bec
Deepak Kumar <dpk.0811@gmail.com>
2022-11-01 15:34:44 -0700

    Pass the commandline argument to an internal function

--- helloWorld/helloWorld.cpp
+++ helloWorld/helloWorld.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+
+void display(char* value){
+    std:: cout << value << "\n";
+}
+
 int main(int argc, char** argv) {
     std::cout << "Hello World!\n";
     std::cout << "Argument count: " << argc - 1 << "\nValue(s): \n";
     for(int i = 1; i < argc; ++i){
-        std::cout << argv[i] << "\n";
+        display(argv[i]);
     }
     return 0;
 }
