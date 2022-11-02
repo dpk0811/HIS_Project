d694795f8594b4c4a5fb977388125e29d1470cb3
Deepak Kumar <dpk.0811@gmail.com>
2022-11-02 12:44:59 -0700

    Changes to simulate stack overflow

--- helloWorld/helloWorld.cpp
+++ helloWorld/helloWorld.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include<cstring>
 
 
-void display(char* value){
-    std:: cout << value << "\n";
+void display(char *values){
+    char array[32];
+    std:: strcpy(array, values);
+    std:: cout << array << "\n";
+    std:: cout << "Size: " << sizeof(array) << "\n";
 }
 
 int main(int argc, char** argv) {
@@ -11,5 +15,6 @@ int main(int argc, char** argv) {
     for(int i = 1; i < argc; ++i){
         display(argv[i]);
     }
+    std:: cout << "Program terminated without errors!";
     return 0;
 }
